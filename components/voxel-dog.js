035f48f593ed06327e001e7f73d33538377a2118
Ollie <oquarm@student.unimelb.edu.au>
2023-06-27 23:29:55 -0500

    deployment changes

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,7 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://olliequ.github.io' : '') + '/dog.glb'
+  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://olliequ.github.io' : '') + '/static/images/dog.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
